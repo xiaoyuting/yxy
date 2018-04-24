5d53157ea4393d9eb6874b1d2436507f78940a35
xiaoyuting <479072483@qq.com>
2018-04-24 19:53:44 +0800

    serves

---
+++ ProjectTemplate/GiftGroup/giftCell.h
@@ -0,0 +1,13 @@
+//
+//  giftCell.h
+//  ProjectTemplate
+//
+//  Created by GM on 2018/4/24.
+//  Copyright © 2018年 yuting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface giftCell : UITableViewCell
+
+@end
---
+++ ProjectTemplate/GiftGroup/giftCell.m
@@ -0,0 +1,24 @@
+//
+//  giftCell.m
+//  ProjectTemplate
+//
+//  Created by GM on 2018/4/24.
+//  Copyright © 2018年 yuting. All rights reserved.
+//
+
+#import "giftCell.h"
+
+@implementation giftCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
